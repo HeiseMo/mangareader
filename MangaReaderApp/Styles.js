823ae7c060a77b4519cbb03b0224f002d8345d18
Daniel Tanurhan <danieltanurhan@gmail.com>
2024-03-13 14:38:48 -0700

    fixed styles and settings

--- MangaReaderApp/Styles.js
+++ MangaReaderApp/Styles.js
@@ -30,38 +30,36 @@ const styles = (colorScheme, screenWidth) => StyleSheet.create({
       fontSize: 18,
       fontWeight: 'bold',
       marginTop: 10,
-  },
-  chapterItem: {
-      fontSize: 16,
-      marginTop: 5,
-  },
-  chapterList: {
-    marginTop: 10,
-},
-chapterItem: {
-    backgroundColor: '#f0f0f0',
-    padding: 10,
-    borderRadius: 5,
-    marginVertical: 5,
-    marginHorizontal: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-},
-chapterTitle: {
-    fontSize: 16,
-},
-  imageContainer: {
-    alignItems: 'center', // Ensure images are stretched to fill the width, if not already by the image styles
-  },
-  chapterImage: {
-    width: screenWidth, // Ensure the image fills the width
-    resizeMode: 'contain', // Maintain the aspect ratio. Consider 'cover' if you want images to fill the screen fully.
-  },
+    },
+    chapterList: {
+      marginTop: 10,
+    },
+    chapterItem: {
+        backgroundColor: '#f0f0f0',
+        padding: 10,
+        fontSize: 16,
+        marginTop: 5,
+        borderRadius: 5,
+        marginVertical: 5,
+        marginHorizontal: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    chapterTitle: {
+        fontSize: 16,
+    },
+    imageContainer: {
+      alignItems: 'center', // Ensure images are stretched to fill the width, if not already by the image styles
+    },
+    chapterImage: {
+      width: screenWidth, // Ensure the image fills the width
+      resizeMode: 'contain', // Maintain the aspect ratio. Consider 'cover' if you want images to fill the screen fully.
+    },
   // Consider adding styles for the scrollView to manage the overall background and alignment
-  scrollView: {
-    backgroundColor: '#000', // Optional, consider setting a background color that matches your app theme or images
-  },
+    scrollView: {
+      backgroundColor: '#000', // Optional, consider setting a background color that matches your app theme or images
+    },
 });
 
 export default styles;
